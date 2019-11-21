5f2afa021d66f99aa756e830334a1cef00b7c1ff
Alexander Gill <aasg20@bath.ac.uk>
2019-11-21 16:10:11 +0000

    create crimmins filter

---
+++ crimmins-comp-hulling-filter.py
@@ -0,0 +1,9 @@
+iterations = 1
+
+directions = [[1,0], [1,1], [0,1], [-1,1]]
+
+for i in range(1, iterations):
+    # dark pixel adjustment
+    for direction in directions:
+        for i, row in enumerate(imageArray):
+            pass
